6891e991e194c2a7c127332962cef398d3ba26ff
Shrike <Jschwass@outlook.com>
2022-05-04 11:21:30 +1200

    3.3

--- 03_Quiz_GUI.py
+++ 03_Quiz_GUI.py
@@ -21,10 +21,6 @@ class Convertor:
         # Background Formatting
         background_color = "light blue"
         background_color2 = "light blue"
-        b1bg = "red"
-        b2bg = "blue"
-        b3bg = "yellow"
-        b4bg = "green"
 
         # Main Screen
         self.convertor_frame = Frame(width=300, height=300,
@@ -56,29 +52,23 @@ class Convertor:
                                           padx=10, pady=10)
         self.temp_convertor_label.grid(row=2, column=0)
 
-        # Quiz Buttons Frame
-        self.quiz_frame = Frame(self.convertor_frame,
-                                width=300, height=300,
-                                bg=background_color2, pady=10)
-        self.quiz_frame.grid()
-
         # Quiz Buttons 1-4
-        self.b1_button = Button(self.quiz_frame, text=Ans1,
+        self.b1_button = Button(self.convertor_frame, text=Ans1,
                                 font=("Arial", "10"),
-                                padx=30, pady=10, bg=b1bg)
-        self.b1_button.grid(row=1, column=1)
-        self.b2_button = Button(self.quiz_frame, text=Ans2,
+                                padx=30, pady=10)
+        self.b1_button.grid(row=3, column=0)
+        self.b2_button = Button(self.convertor_frame, text=Ans2,
                                 font=("Arial", "10"),
-                                padx=30, pady=10, bg=b2bg)
-        self.b2_button.grid(row=1, column=2)
-        self.b3_button = Button(self.quiz_frame, text=Ans3,
+                                padx=30, pady=10)
+        self.b2_button.grid(row=3, column=1)
+        self.b3_button = Button(self.convertor_frame, text=Ans3,
                                 font=("Arial", "10"),
-                                padx=30, pady=10, bg=b3bg)
-        self.b3_button.grid(row=2, column=1)
-        self.b4_button = Button(self.quiz_frame, text=Ans4,
+                                padx=30, pady=10)
+        self.b3_button.grid(row=4, column=0)
+        self.b4_button = Button(self.convertor_frame, text=Ans4,
                                 font=("Arial", "10"),
-                                padx=30, pady=10, bg=b4bg)
-        self.b4_button.grid(row=2, column=2)
+                                padx=30, pady=10)
+        self.b4_button.grid(row=4, column=1)
 
 
 # Main Retinue

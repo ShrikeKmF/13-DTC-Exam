6558d8b51117187091849c1260154b4d38ccaba3
Shrike <Jschwass@outlook.com>
2022-05-09 10:03:30 +1200

    8

--- 03_Quiz_GUI_v5.py
+++ 03_Quiz_GUI_v5.py
@@ -18,7 +18,6 @@ b3List = [22, 10, ]
 b4List = [18, 19, ]
 userAns = 0
 correctAns = 0
-state = 0
 
 
 # Answer Checker
@@ -38,211 +37,204 @@ def AnswerChecker(userAns):
         print("Log: New Question Num {}".format(questionNum))
 
 
-# Changing gamestate once max question have been reached
-if questionNum == 11:
-    state = 2
+class Start:
+    def __init__(self):
+        # Background Formatting
+        background_color = "light blue"
 
-if state == 0:
-    class start:
-        def __init__(self):
-            # Background Formatting
-            background_color = "light blue"
+        # Main Screen
+        self.start_frame = Frame(width=300, height=300,
+                                     bg=background_color, pady=10)
+        self.start_frame.grid()
 
-            # Main Screen
-            self.start_frame = Frame(width=300, height=300,
-                                         bg=background_color, pady=10)
-            self.start_frame.grid()
-
-            # Title
-            self.temp_convertor_label = Label(self.start_frame,
-                                              text="Te Reo Maori Quiz",
-                                              font=("Arial", "16", "bold"),
-                                              bg=background_color,
-                                              padx=10, pady=10)
-            self.temp_convertor_label.grid(row=0)
-
-            # Start Button
-            self.start_button = Button(self.start_frame, text="Start",
-                                      font=("Arial", "14"),
-                                      padx=10, pady=10, command=self.startQuiz)
-            self.start_button.grid(row=1)
-
-        # Start Command
-        def startQuiz(self):
-            global state
-            print("Log: Start Quiz")
-            state = 1
-            print("Log:", state)
-
-
-if state == 1:
-    class quiz:
-        def __init__(self):
-            background_color = "light blue"  # Background Color Formatting
-            background_color2 = "light blue"  # Background Color Formatting
-            b1bg = "red"  # Button Color Formatting
-            b2bg = "blue"  # Button Color Formatting
-            b3bg = "yellow"  # Button Color Formatting
-            b4bg = "green"  # Button Color Formatting
-            helpbg = "orange"  # Help Background Color Formatting
-
-            # Main Screen
-            self.quiz_frame = Frame(width=300, height=300,
-                                    bg=background_color, pady=10)
-            self.quiz_frame.grid()
-
-            # Title
-            self.temp_title_label = Label(self.quiz_frame,
-                                          text="Te Reo Maori Number Quiz",
-                                          font=("Arial", "24", "bold"),
+        # Title
+        self.temp_convertor_label = Label(self.start_frame,
+                                          text="Te Reo Maori Quiz",
+                                          font=("Arial", "16", "bold"),
                                           bg=background_color,
-                                          padx=50, pady=10)
-            self.temp_title_label.grid(row=0, column=0)
+                                          padx=10, pady=10)
+        self.temp_convertor_label.grid(row=0)
 
-            # Question Number
-            self.temp_questionnum_label = Label(self.quiz_frame,
-                                                text="Question {}".format(
-                                                    (questionNum + 1)),
-                                                font=("Arial", "12", "bold"),
-                                                bg=background_color,
-                                                padx=10, pady=10)
-            self.temp_questionnum_label.grid(row=1, column=0)
+        # Start Button
+        self.start_button = Button(self.start_frame, text="Start",
+                                  font=("Arial", "14"),
+                                  padx=10, pady=10, command=self.startQuiz)
+        self.start_button.grid(row=1)
 
-            # Question Text
-            self.temp_questiontxt_label = Label(self.quiz_frame,
-                                                text=QuestionTxtList[
-                                                    questionNum],
-                                                font=("Arial", "12", "bold"),
-                                                bg=background_color,
-                                                padx=10, pady=10)
-            self.temp_questiontxt_label.grid(row=2, column=0)
-
-            # Quiz Buttons Frame
-            self.button_frame = Frame(self.quiz_frame,
-                                      width=300, height=300,
-                                      bg=background_color2, pady=10)
-            self.button_frame.grid()
-
-            # Quiz Buttons 1-4
-            self.b1_button = Button(self.button_frame,
-                                    text=b1List[questionNum],
-                                    font=("Arial", "10"),
-                                    padx=30, pady=10, bg=b1bg,
-                                    command=self.answerB1)
-            self.b1_button.grid(row=1, column=1)
-            self.b2_button = Button(self.button_frame,
-                                    text=b2List[questionNum],
-                                    font=("Arial", "10"),
-                                    padx=30, pady=10, bg=b2bg,
-                                    command=self.answerB2)
-            self.b2_button.grid(row=1, column=2)
-            self.b3_button = Button(self.button_frame,
-                                    text=b3List[questionNum],
-                                    font=("Arial", "10"),
-                                    padx=30, pady=10, bg=b3bg,
-                                    command=self.answerB3)
-            self.b3_button.grid(row=2, column=1)
-            self.b4_button = Button(self.button_frame,
-                                    text=b4List[questionNum],
-                                    font=("Arial", "10"),
-                                    padx=30, pady=10, bg=b4bg,
-                                    command=self.answerB4)
-            self.b4_button.grid(row=2, column=2)
-
-            # Help Button
-            self.help_button = Button(self.quiz_frame, text="Help",
-                                      font=("Arial", "14"),
-                                      padx=20, pady=10, command=self.help,
-                                      bg=helpbg)
-            self.help_button.grid(row=5, column=0)
-
-        # Help Command Function
-        def help(self):
-            print("Log: Help")
-            get_help = Help(self)
-            get_help.help_text.configure(text="Help text goes here")
-
-        # Answer Command Function 1-4
-        def answerB1(self):  # Button 1 Answer
-            print("Log: Answer 1")
-            userAns = b1List[questionNum]
-            AnswerChecker(userAns)
-            self.UpdateTxt()
-
-        def answerB2(self):  # Button 2 Answer
-            print("Log: Answer 2")
-            userAns = b2List[questionNum]
-            AnswerChecker(userAns)
-            self.UpdateTxt()
-
-        def answerB3(self):  # Button 3 Answer
-            global QuestionTxt
-            global questionAns
-            print("Log: Answer 3")
-            userAns = b3List[questionNum]
-            AnswerChecker(userAns)
-            self.UpdateTxt()
-
-        def answerB4(self):  # Button 4 Answer
-            print("Log: Answer 4")
-            userAns = b4List[questionNum]
-            AnswerChecker(userAns)
-            self.UpdateTxt()
-
-        def UpdateTxt(self):
-            self.temp_questiontxt_label['text'] = QuestionTxtList[questionNum]
-            self.temp_questionnum_label['text'] = "Question", questionNum + 1
-            self.b1_button['text'] = b1List[questionNum]
-            self.b2_button['text'] = b2List[questionNum]
-            self.b3_button['text'] = b3List[questionNum]
-            self.b4_button['text'] = b4List[questionNum]
+    # Start Command
+    def startQuiz(self):
+        self.start_frame.destroy()
+        self.newWindow = Quiz()
+        print("Log: Start Quiz")
 
 
-    # Help Screen Class
-    class Help:
-        def __init__(self, partner):
-            # Formatting background
-            background = "orange"
+class Quiz:
+    def __init__(self):
+        background_color = "light blue"  # Background Color Formatting
+        background_color2 = "light blue"  # Background Color Formatting
+        b1bg = "red"  # Button Color Formatting
+        b2bg = "blue"  # Button Color Formatting
+        b3bg = "yellow"  # Button Color Formatting
+        b4bg = "green"  # Button Color Formatting
+        helpbg = "orange"  # Help Background Color Formatting
 
-            partner.help_button.config(state=DISABLED)
+        # Main Screen
+        self.quiz_frame = Frame(width=300, height=300,
+                                bg=background_color, pady=10)
+        self.quiz_frame.grid()
 
-            # Forcing to Top Level
-            self.help_box = Toplevel()
+        # Title
+        self.temp_title_label = Label(self.quiz_frame,
+                                      text="Te Reo Maori Number Quiz",
+                                      font=("Arial", "24", "bold"),
+                                      bg=background_color,
+                                      padx=50, pady=10)
+        self.temp_title_label.grid(row=0, column=0)
 
-            # Stop unwanted Windows
-            self.help_box.protocol('WM_DELETE_WINDOWS',
-                                   partial(self.close_help, partner))
+        # Question Number
+        self.temp_questionnum_label = Label(self.quiz_frame,
+                                            text="Question {}".format(
+                                                (questionNum + 1)),
+                                            font=("Arial", "12", "bold"),
+                                            bg=background_color,
+                                            padx=10, pady=10)
+        self.temp_questionnum_label.grid(row=1, column=0)
 
-            # Frame Creation
-            self.help_frame = Frame(self.help_box, width=300, bg=background)
-            self.help_frame.grid()
+        # Question Text
+        self.temp_questiontxt_label = Label(self.quiz_frame,
+                                            text=QuestionTxtList[
+                                                questionNum],
+                                            font=("Arial", "12", "bold"),
+                                            bg=background_color,
+                                            padx=10, pady=10)
+        self.temp_questiontxt_label.grid(row=2, column=0)
 
-            # Help Title
-            self.how_heading = Label(self.help_frame, text="Help",
-                                     font="arial 16 bold", bg=background)
-            self.how_heading.grid(row=0)
+        # Quiz Buttons Frame
+        self.button_frame = Frame(self.quiz_frame,
+                                  width=300, height=300,
+                                  bg=background_color2, pady=10)
+        self.button_frame.grid()
 
-            # Help Text
-            self.help_text = Label(self.help_frame, text="", justify="left",
-                                   width=40, bg=background, wrap=250)
-            self.help_text.grid(row=1)
+        # Quiz Buttons 1-4
+        self.b1_button = Button(self.button_frame,
+                                text=b1List[questionNum],
+                                font=("Arial", "10"),
+                                padx=30, pady=10, bg=b1bg,
+                                command=self.answerB1)
+        self.b1_button.grid(row=1, column=1)
+        self.b2_button = Button(self.button_frame,
+                                text=b2List[questionNum],
+                                font=("Arial", "10"),
+                                padx=30, pady=10, bg=b2bg,
+                                command=self.answerB2)
+        self.b2_button.grid(row=1, column=2)
+        self.b3_button = Button(self.button_frame,
+                                text=b3List[questionNum],
+                                font=("Arial", "10"),
+                                padx=30, pady=10, bg=b3bg,
+                                command=self.answerB3)
+        self.b3_button.grid(row=2, column=1)
+        self.b4_button = Button(self.button_frame,
+                                text=b4List[questionNum],
+                                font=("Arial", "10"),
+                                padx=30, pady=10, bg=b4bg,
+                                command=self.answerB4)
+        self.b4_button.grid(row=2, column=2)
 
-            # Close Button
-            self.dismiss_btn = Button(self.help_frame, text="Close", width=10,
-                                      bg="orange", font="Arial 10 bold",
-                                      command=partial(self.close_help,
-                                                      partner))
-            self.dismiss_btn.grid(row=2, pady=10)
+        # Help Button
+        self.help_button = Button(self.quiz_frame, text="Help",
+                                  font=("Arial", "14"),
+                                  padx=20, pady=10, command=self.help,
+                                  bg=helpbg)
+        self.help_button.grid(row=5, column=0)
 
-        # Close Command
-        def close_help(self, partner):
-            partner.help_button.config(state=NORMAL)
-            self.help_box.destroy()
+    # Help Command Function
+    def help(self):
+        print("Log: Help")
+        get_help = Help(self)
+        get_help.help_text.configure(text="Help text goes here")
+
+    # Answer Command Function 1-4
+    def answerB1(self):  # Button 1 Answer
+        print("Log: Answer 1")
+        userAns = b1List[questionNum]
+        AnswerChecker(userAns)
+        self.UpdateTxt()
+
+    def answerB2(self):  # Button 2 Answer
+        print("Log: Answer 2")
+        userAns = b2List[questionNum]
+        AnswerChecker(userAns)
+        self.UpdateTxt()
+
+    def answerB3(self):  # Button 3 Answer
+        global QuestionTxt
+        global questionAns
+        print("Log: Answer 3")
+        userAns = b3List[questionNum]
+        AnswerChecker(userAns)
+        self.UpdateTxt()
+
+    def answerB4(self):  # Button 4 Answer
+        print("Log: Answer 4")
+        userAns = b4List[questionNum]
+        AnswerChecker(userAns)
+        self.UpdateTxt()
+
+    def UpdateTxt(self):
+        self.temp_questiontxt_label['text'] = QuestionTxtList[questionNum]
+        self.temp_questionnum_label['text'] = "Question", questionNum + 1
+        self.b1_button['text'] = b1List[questionNum]
+        self.b2_button['text'] = b2List[questionNum]
+        self.b3_button['text'] = b3List[questionNum]
+        self.b4_button['text'] = b4List[questionNum]
+
+
+# Help Screen Class
+class Help:
+    def __init__(self, partner):
+        # Formatting background
+        background = "orange"
+
+        partner.help_button.config(state=DISABLED)
+
+        # Forcing to Top Level
+        self.help_box = Toplevel()
+
+        # Stop unwanted Windows
+        self.help_box.protocol('WM_DELETE_WINDOWS',
+                               partial(self.close_help, partner))
+
+        # Frame Creation
+        self.help_frame = Frame(self.help_box, width=300, bg=background)
+        self.help_frame.grid()
+
+        # Help Title
+        self.how_heading = Label(self.help_frame, text="Help",
+                                 font="arial 16 bold", bg=background)
+        self.how_heading.grid(row=0)
+
+        # Help Text
+        self.help_text = Label(self.help_frame, text="", justify="left",
+                               width=40, bg=background, wrap=250)
+        self.help_text.grid(row=1)
+
+        # Close Button
+        self.dismiss_btn = Button(self.help_frame, text="Close", width=10,
+                                  bg="orange", font="Arial 10 bold",
+                                  command=partial(self.close_help,
+                                                  partner))
+        self.dismiss_btn.grid(row=2, pady=10)
+
+    # Close Command
+    def close_help(self, partner):
+        partner.help_button.config(state=NORMAL)
+        self.help_box.destroy()
 
 # Main Retinue
 if __name__ == "__main__":
     root = Tk()
     root.title("Te Reo Maori Quiz")
-    something = start()
+    something = Start()
     root.mainloop()
 print("Log: Number Correct", correctAns, "/ 12")
